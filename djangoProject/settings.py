2f4f4d41317e93dadfc2f7149c4bb21b050545d2
Chiara <gori.chiara@yahoo.it>
2023-06-10 16:26:57 +0200

    fixed ALLOWED_HOSTS for deployment

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -38,7 +38,8 @@ SECRET_KEY = 'django-insecure--@bdmo%4n#*xd#rvd+p(^61pf477a$kquij!d&m^77^h1klj8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'djangoproject-production-9641.up.railway.app']
+
 
 
 # Application definition
@@ -139,12 +140,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
