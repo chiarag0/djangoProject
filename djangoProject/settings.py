d6fc6e9b02b9aac1962346b9aeadde801862b9f4
Chiara <gori.chiara@yahoo.it>
2023-06-12 16:01:25 +0200

    Final commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -34,8 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@bdmo%4n#*xd#rvd+p(^61pf477a$kquij!d&m^77^h1klj8-'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure--@bdmo%4n#*xd#rvd+p(^61pf477a$kquij!d&m^77^h1klj8-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
