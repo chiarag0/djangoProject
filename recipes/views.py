ef908ecd655f1836c5e9f9179e873e2726b19deb
Chiara Gori <71407980+chiarag0@users.noreply.github.com>
2023-06-12 11:08:16 +0200

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -42,6 +42,7 @@ class RecipeDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['ingredients'] = Ingredient.objects.filter(recipe=self.object)
+        context['instructions'] = models.Instruction.objects.filter(recipe=self.object)
         return context
 
 
